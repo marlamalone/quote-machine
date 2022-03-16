63e6038f1373a587f8b6c49ade56db04df8d6979
marlamalone <marla.r.malone@gmail.com>
2022-03-16 14:42:26 -0500

    3 files modified

---
+++ App.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default ({ title, author, imglink, link, quote }) => <h1>{title}</h1>;
--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './style.css';
-import data from 'https://github.com/marlamalone/marlamalone.github.io/blob/1d585914ee6febaed505cc3cea065a4281896b96/quotes-data-alltime-clean.json';
+import { data, title, author_name, post_image, link, top_quote } from 'https://github.com/marlamalone/marlamalone.github.io/blob/1d585914ee6febaed505cc3cea065a4281896b96/quotes-data-alltime-clean.json';
 
 var currentQuote = '',
   currentAuthor = '';
@@ -10,22 +10,30 @@ function getRandomQuote() {
   return data[Math.floor(Math.random() * data.length)];
 }
 
-function getQuote() {
-  let randomQuote = getRandomQuote();
-  console.log(randomQuote);
-  currentQuote = randomQuote.top_quote;
-  currentAuthor = randomQuote.author_name;
+interface AppProps { }
+interface AppState {
+  title: string,
+  author: string,
+  imglink: string,
+  title: string,
+  quote: string;
 }
 
-/* class QuoteDisplay extends React.Component {
+const quoteBox = document.getElementById("quote-box");
+const quoteText = document.getElementById("quote-text");
+const quoteAuthor = document.getElementById("quote-author");
+
+quoteText.textContent = 'test';
+
+class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
-    this.quote = { quote: currentQuote };
-    this.author = { author: currentAuthor };
-  }
-
-  render() {
-    return <div>{this.quote}</div>;
-  }
-}
-ReactDOM.render(<QuoteDisplay />, document.getElementById('root'));*/
+    this.state = {
+      title: { this.data.title },
+      author: { this.data.author_name },
+      imglink: { this.data.post_image },
+      link: { this.data.link },
+      quote: { this.data.top_quote }
+    };
+  
+render(<App />, document.getElementById('root'));
